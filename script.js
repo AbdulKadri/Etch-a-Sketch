66ffed48f12663e791a92e22ed122790538465e2
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2022-07-03 02:34:09 -0600

    Update rainbow and erase btn

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const defaultColor = '#FF0000'
-let newColor = defaultColor;
-let cells;
+let currentButton = 'color'
 
 const grid = document.getElementById('grid');
-const colorChoice = document.getElementById('colorChoice')
+const colorChoice = document.getElementById('colorChoice');
 const chooseColor = document.getElementById('chooseColor');
 const RGB = document.getElementById('RGB');
 const erase = document.getElementById('erase');
@@ -11,13 +9,14 @@ const clear = document.getElementById('clear');
 const sizeValue = document.getElementById('sizeValue');
 const sideSlider = document.getElementById('sizeSlider');
 
-colorChoice.oninput = (e) => setColor(e.target.value);
+// colorChoice.oninput = (e) => setColor(e.target.value);
+erase.onclick = () => setColor('erase')
+chooseColor.onclick = () => setColor('color')
+RGB.onclick = () => setColor('rainbow')
 
-function setColor(value) {
-    newColor = value;
-    // return newColor;
+function setColor(newButton) {
+    currentButton = newButton
 }
-
 
 // Creating the grid
 function makeGrid(side) {
@@ -25,36 +24,36 @@ function makeGrid(side) {
     grid.style.gridTemplateRows = `repeat(${side}, 1fr)`
 
     for (let x = 0; x < side ** 2; x++) {
-        const gridPixel = document.createElement('div');
-        gridPixel.setAttribute('id', 'pixels');
-        gridPixel.addEventListener('mouseover', changeColor);
-        // gridPixel.addEventListener('mousedown', changeColor);
-        grid.appendChild(gridPixel);
+        const gridPixel = document.createElement('div')
+        gridPixel.setAttribute('id', 'pixels')
+        gridPixel.addEventListener('mouseover', changeColor)
+        gridPixel.addEventListener('mousedown', changeColor)
+        grid.appendChild(gridPixel)
     }
 };
 
-makeGrid(32);
-
 //Only allow colouring if mouse is clicked down and dragged across the box
-// must add another function with code below
-// if (e.type === 'mouseover' && !mouseClick) return;
 let mouseClick = false
 document.body.onmousedown = () => (mouseClick = true)
 document.body.onmouseup = () => (mouseClick = false)
 
-function changeColor() {
-    switch (defaultColor) {
-        case 'rainbow':
-            this.style.backgroundColor = rainbow();
-            break;
-        case 'erase':
-            this.style.backgroundColor = 'white';
-            console.log('hi')
-            break;
-        case 'color':
-            this.style.backgroundColor = newColor;
-            break;
-        default:
-            this.style.backgroundColor = defaultColor;
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseClick) return
+    // e.target.style.backgroundColor = '#FF0000'
+    if (currentButton === 'color') {
+        e.target.style.backgroundColor = '#FF0000'
+    } else if (currentButton === 'erase') {
+        e.target.style.backgroundColor = '#FFFFFF'
+    } else if (currentButton === 'rainbow') {
+        const amountRed = Math.floor(Math.random() * 256)
+        const amountGreen = Math.floor(Math.random() * 256)
+        const amountBlue = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${amountRed}, ${amountGreen}, ${amountBlue})`
     }
+}
+
+console.log(currentButton)
+
+window.onload = () => {
+    makeGrid(16)
 }
